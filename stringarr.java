bd9c04973bab710fc916a6f8d8e76eda10d18437
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-05 22:57:10 +0530

    to print string

---
+++ stringarr.java
@@ -0,0 +1,10 @@
+package array;
+import java.util.Arrays;
+
+public class stringarr {
+public static void main(String[] args) {
+	String arr[]= {"cars","bike","scootet"};
+	System.out.println(Arrays.toString(arr));
+	
+}
+}
